477d8b957105ee16317e46baa5c6b97e9d098c1b
NeilDG <NeilDG@github.com>
2017-09-02 14:35:53 +0800

    Setup for infinite scrolling.

---
+++ Assets/Scripts/UI Optimization/ScrollableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	/// <summary>
+	/// Moves this oject to its last index
+	/// </summary>
+	public void MoveToLast() {
+		this.transform.SetSiblingIndex (this.transform.parent.childCount - 1);
+	}
+
+
+	/// <summary>
+	/// Moves this object to its first index
+	/// </summary>
+	public void MoveToFirst() {
+		this.transform.SetSiblingIndex (0);
+	}
+
+}
