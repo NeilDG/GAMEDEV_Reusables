a2e4ae944f132587e4fa04c4aab1232f3094ac09
NeilDG <NeilDG@github.com>
2020-06-22 21:44:50 +0800

    Added standard assets back.

---
+++ Assets/Scripts/Snowman/SpawnDemo_1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Spawns a template object at different locations
+public class SpawnDemo_1 : MonoBehaviour
+{
+    [SerializeField] private GameObject templateObject;
+    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject[] spawnLocations;
+
+    private GameObject activeObject;
+    private float ticks = 0.0f;
+    private float SPAWN_INTERVAL = 1.0f;
+    private int spawnIndex = 0;
+     
+    //Awake is called earlier than start
+    private void Awake()
+    {
+        this.templateObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.ticks += Time.deltaTime;
+        if(this.ticks > this.SPAWN_INTERVAL)
+        {
+            this.ticks = 0.0f;
+            this.SPAWN_INTERVAL = Random.Range(0.5f, 1.5f);
+
+            if(this.activeObject != null)
+            {
+                GameObject.Destroy(this.activeObject);
+                this.activeObject = null;
+            }
+            this.activeObject = this.SpawnDefault();
+
+            Vector3 myLocation = this.activeObject.transform.localPosition;
+            Vector3 newLocation = this.spawnLocations[this.spawnIndex].transform.localPosition;
+            myLocation.x = newLocation.x;
+            myLocation.z = newLocation.z;
+            this.activeObject.transform.localPosition = myLocation;
+
+            this.spawnIndex++; this.spawnIndex %= this.spawnLocations.Length;
+        }
+    }
+
+    private GameObject SpawnDefault()
+    {
+        GameObject myObj = GameObject.Instantiate(this.templateObject, this.parent);
+        myObj.SetActive(true);
+        return myObj;
+    }
+}
