5c47d4cce3a905d9bc3a74901a38383f0c85f79c
NeilDG <NeilDG@github.com>
2020-10-04 17:02:30 +0800

    Reseting project to stable state for new course.

---
+++ Assets/Scripts/Snowman/SnowmanController_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Manager class or a data holder class. Utility class
+public class SnowmanController_2 : MonoBehaviour
+{
+    [SerializeField] private int serializeValue = 10; //limited to your inspector window
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void hello(int x)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
